421ac2080e8d9a6df4ebb21592123d71d048cbd2
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2024-10-04 01:17:09 +0530

    style(login): design Login Page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,29 @@
-import SignInForm from "@/components/forms/login";
+import Image from "next/image";
+import Link from "next/link";
 
-const SignInPage = () => {
+import LoginForm from "@/components/forms/login";
+
+const LoginPage = () => {
   return (
-    <>
-      <h5 className="text-themeTextWhite text-base font-bold">Login</h5>
-      <p className="text-themeTextGray leading-tight">Welcome Back Ansopedian!</p>
-      <SignInForm />
-      <div className="relative my-10 w-full">
-        <div className="text-themeTextGray absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform bg-black p-3 text-xs">
-          OR CONTINUE WITH
-        </div>
+    <div className="container m-auto flex h-dvh w-dvw items-center justify-center gap-6 p-6">
+      <div className="lg:1/4 w-full sm:w-2/3 md:w-2/4 lg:w-1/3">
+        <h5 className="text-themeTextWhite text-base font-bold">Login</h5>
+        <p className="text-themeTextGray leading-tight">
+          Welcome Back <span className="text-orange-500">Ansopedian!</span>
+        </p>
+        <LoginForm />
+        <p className="mt-4">
+          Don&apos;t have an account yet?
+          <Link href="/signup" className="text-blue-500 hover:underline">
+            Sign up
+          </Link>
+        </p>
       </div>
-    </>
+      <div className="hidden items-center justify-center sm:block sm:w-2/3 md:w-2/3 lg:w-1/3">
+        <Image src="/images/login-illustrator.svg" alt="login" width={500} height={500} />
+      </div>
+    </div>
   );
 };
 
-export default SignInPage;
+export default LoginPage;
